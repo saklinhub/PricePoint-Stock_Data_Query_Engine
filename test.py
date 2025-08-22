bd4507159927940c212a857ed4391cb00a162621
saklin <saklinmostak321@gmail.com>
2025-08-23 00:30:32 +0530

    stock_market.db excluded

--- test.py
+++ test.py
@@ -7,12 +7,12 @@
 # df = pd.read_csv('stocks.csv')  # Or 'all_stocks_5yr.csv'
 # print(df.columns)
 
-# import pandas as pd
-# df = pd.read_csv('all_stocks_5yr.csv')  # Or your CSV file
-# df.rename(columns={'date': 'Date', 'Name': 'Ticker', 'open': 'Open', 'high': 'High', 'low': 'Low', 'close': 'Close', 'volume': 'Volume'}, inplace=True)
-# df.to_csv('stocks.csv', index=False)
-
 import pandas as pd
-df = pd.read_csv('stocks.csv')  # Or 'all_stocks_5yr.csv'
-print(df.columns)
-print(df.head())
+df = pd.read_csv('all_stocks_5yr.csv')  # Or your CSV file
+df.rename(columns={'date': 'Date', 'Name': 'Ticker', 'open': 'Open', 'high': 'High', 'low': 'Low', 'close': 'Close', 'volume': 'Volume'}, inplace=True)
+df.to_csv('stocks.csv', index=False)
+
+# import pandas as pd
+# df = pd.read_csv('stocks.csv')  # Or 'all_stocks_5yr.csv'
+# print(df.columns)
+# print(df.head())
